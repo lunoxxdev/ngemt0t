02e6a56d8c79fc044d46bdfb9df80033d444a4f0
lunoxxdev <128571018+lunoxxdev@users.noreply.github.com>
2023-03-23 00:01:07 +0700

    Update start.py

--- chatgpt/module/start.py
+++ chatgpt/module/start.py
@@ -14,5 +14,5 @@ from pyrogram.types import *
 @ren.on_message(filters.command("start") & filters.private)
 async def start_bot(c: Client, m: Message):
     start_welcome = f"Hallo Cok ! {m.from_user.mention}\n\nTanya Apapun Bebas Asal Gak Bahas Agama, Ras, Suku Atau Tak Bal Ndasmu 😠.\n\nPaham Ra Cok??\n\nContoh : `/tanya membuat anak`"
-    start_button = InlineKeyboardMarkup([[InlineKeyboardButton("Join Grub Ku Cok", url=f"https://t.me/{c.internetgratishoax.username}?startgroup=True")]])
+    start_button = InlineKeyboardMarkup([[InlineKeyboardButton("Join Grub Ku Cok", url=f"@internetgratishoax{c.me.username}?startgroup=True")]])
     await m.reply_text(start_welcome, reply_markup=start_button)
